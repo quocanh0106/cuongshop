494f20d0d228306f563d224bb713e43acf72a75e
quocanh0106 <quocanh1699@gmail.com>
2021-02-28 15:05:49 +0700

    add theme

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('my-home', 'HomeController@myHome');
+
+Route::get('my-users', 'HomeController@myUsers');
